e9899a51868866a6f63cae1d81fa07e6ead9ff9c
Unknown <migimasa@gmail.com>
2018-07-26 09:13:28 -0500

    Gutter Game

---
+++ BowlingGameKata.Tests/BowlingGameTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BowlingGameKata.Tests
+{
+    public class BowlingGameTest
+    {
+        [Fact]
+        public void TestGutterGame()
+        {
+            Game g = new Game();
+
+            for (int i = 0; i < 20; i++)
+                g.Roll(0);
+
+            Assert.Equal(0, g.Score());
+        }
+    }
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
